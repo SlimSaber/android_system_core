7fcae54d4e147c852de98d87e45e48626b92c655
Benoit Goby <benoit@android.com>
2013-02-20 15:04:53 -0800

    adb: Cleanup dead code
    dns_service is unused and recover_service has been replaced by adb
    sideload
    
    Change-Id: Ie90000d7f672e8299ee1622a9690c7371b214dc1

--- adb/Android.mk
+++ adb/Android.mk
@@ -64,7 +64,6 @@ LOCAL_SRC_FILES := \
 	file_sync_client.c \
 	$(EXTRA_SRCS) \
 	$(USB_SRCS) \
-	utils.c \
 	usb_vendors.c
 
 LOCAL_C_INCLUDES += external/openssl/include
@@ -125,8 +124,7 @@ LOCAL_SRC_FILES := \
 	framebuffer_service.c \
 	remount_service.c \
 	usb_linux_client.c \
-	log_service.c \
-	utils.c
+	log_service.c
 
 LOCAL_CFLAGS := -g -DADB_HOST=0 -Wall -Wno-unused-parameter
 # adb can't be built without optimizations, so we enforce -O2 if no
@@ -181,7 +179,6 @@ LOCAL_SRC_FILES := \
 	file_sync_client.c \
 	get_my_path_linux.c \
 	usb_linux.c \
-	utils.c \
 	usb_vendors.c \
 	fdevent.c
 
